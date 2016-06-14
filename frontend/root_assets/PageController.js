d6b84a5a682da7443e3ac9d3f122adfa0ac53ec7
Alexei <arobu@pycoding.biz>
2016-06-14 16:28:26 +0300

    Add jquery library

---
+++ frontend/root_assets/PageController.js
@@ -0,0 +1,5 @@
+/**
+ * Created by alexei on 6/14/16.
+ */
+
+window.alert("Hello");
--- frontend/root_assets/sayHello.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by alexei on 6/13/16.
- */
-// window.alert("Hello")
-
-var onClickalert = function(){
-  window.alert("Noroc")
-  
-};
